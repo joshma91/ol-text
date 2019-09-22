1046584d28e7698e11cf94c33196d55cb5da8011
joshma91 <josh.ma91@gmail.com>
2019-09-22 16:47:08 -0700

    add Table component tests

---
+++ src/test/table.test.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Table from "../components/Table";
+
+describe("Testing Table component", () => {
+  const testFile = {
+    counts: {
+      test: 50,
+      words: 4
+    },
+    totalCount: 54
+  };
+
+  // Empty implementation of window.scrollTo to supress jsdom error
+  const noop = () => {};
+  Object.defineProperty(window, "scrollTo", { value: noop, writable: true });
+
+  test("component is empty when fileInfo is null", () => {
+    render(<Table fileInfo={null} />);
+
+    // fileInfo is null so table should not render
+    expect(document.querySelector("table")).not.toBeInTheDocument();
+  });
+
+  test("component has proper number of child table elements", () => {
+    render(<Table fileInfo={testFile} />);
+
+    // There should be 3 <tr> child elements
+    expect(document.querySelector("tbody").childNodes.length).toEqual(3);
+  });
+
+  test("component should show fileInfo data", () => {
+    const { getByText } = render(<Table fileInfo={testFile} />);
+
+      expect(getByText("Total Words")).toBeInTheDocument();
+      expect(getByText("54")).toBeInTheDocument();
+      expect(getByText("test")).toBeInTheDocument();
+      expect(getByText("50")).toBeInTheDocument();
+      expect(getByText("words")).toBeInTheDocument();
+      expect(getByText("4")).toBeInTheDocument();
+  });
+
+
+  test("should call window.scrollTo when supplied with data", () => {
+    // Access scrolling behaviour through global name space
+    global.scrollTo = jest.fn();
+
+    // Component rendering without data - shouldn't scroll
+    const { rerender } = render(<Table fileInfo={null} />);
+    expect(window.scrollTo).not.toHaveBeenCalled();
+    
+    // Component rendering with data - should scroll
+    rerender(<Table fileInfo={testFile} />);
+    expect(window.scrollTo).toHaveBeenCalled();
+  });
+});
