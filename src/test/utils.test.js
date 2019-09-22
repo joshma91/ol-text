82f2d6eb4dbd26e1a4629866c5d16c25f7f18b09
joshma91 <josh.ma91@gmail.com>
2019-09-22 14:53:59 -0700

    add jest setup file

--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -2,7 +2,6 @@ import { sendFile } from "../utils";
 import testResults from "./testResults.json"
 
 describe("Testing util functions", () => {
-  jest.setTimeout(30000);
   const API_URL =
     "https://wordcount-api.dev.openlaw.io/api/wordcount/v1/upload";
   const FILE_URL = "https://www.w3.org/TR/PNG/iso_8859-1.txt"
