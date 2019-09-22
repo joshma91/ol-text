73365aa83cd4263194ee950985f776dd035aef2c
joshma91 <josh.ma91@gmail.com>
2019-09-22 15:09:06 -0700

    add button enabled tests

--- src/test/button.test.js
+++ src/test/button.test.js
@@ -6,18 +6,24 @@ describe("Testing Button component", () => {
   const notLoadingText = "Analyze";
   const loadingText = "Analyzing";
 
-  test("shows the proper text when button is not loading", () => {
+  test("shows button enabled the proper text when button is not loading", () => {
     const { getByText } = render(<Button loading={false} />);
 
     // Loading is false and we expect the text "Analyze"
     expect(getByText(notLoadingText)).toBeInTheDocument();
+
+    // Button should be enabled
+    expect(document.querySelector('button')).toBeEnabled()
   });
 
-  test("shows the proper text when button is loading", () => {
+  test("shows button disabled and the proper text when button is loading", () => {
     const { getByText } = render(<Button loading={true} />);
 
     // Loading is true and we expect the text "Analyzing"
     expect(getByText(loadingText)).toBeInTheDocument();
+
+    // Button should be disabled
+    expect(document.querySelector('button')).toBeDisabled()
   });
 
   test("invokes the parent callback", () => {
