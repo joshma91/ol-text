4ff261f82e4e6ff2cdd42cd22a5b8f6c4df3b4a4
joshma91 <josh.ma91@gmail.com>
2019-09-22 22:09:06 -0700

    refactor dropzone selector into helpers.js

--- src/test/dropzone.test.js
+++ src/test/dropzone.test.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import Dropzone from '../components/Dropzone'
-import { fireEvent, render, waitForElement } from '@testing-library/react'
+import React from "react";
+import Dropzone from "../components/Dropzone";
+import { fireEvent, render, waitForElement } from "@testing-library/react";
+import { simulateFileDrop } from "./helpers";
 
 describe("Testing Dropzone component", () => {
-  test('show file name after dropping file in component', async () => {
-
-    const file = new File(['test'], 'some.txt', {type: "text/plain"})
-    const { getByText, queryByText, getByTestId } = render(<Dropzone setFiles={jest.fn()}/>)
-    const formElement = getByTestId('dropzone');
-
-    // Define files property on Dropzone element and fire drop event
-    Object.defineProperty(formElement, 'files', { value: [file] });
-    fireEvent.drop(formElement)
+  test("show file name after dropping file in component", async () => {
+    const file = new File(["test"], "some.txt", {type: "text/plain"})
+    const { getByTestId, getByText, queryByText } = render(<Dropzone setFiles={jest.fn()} />);
+    simulateFileDrop(getByTestId, file);
 
     // Wait for DOM change
-    await waitForElement(() => getByText(/some.txt/i))
-  
-    expect(queryByText('some.txt')).toBeDefined()
-  })
-})
+    await waitForElement(() => getByText(/some.txt/i));
+
+    expect(queryByText("some.txt")).toBeDefined();
+  });
+});
---
+++ src/test/helpers.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { fireEvent } from "@testing-library/react";
+
+export const simulateFileDrop = (getByTestId, file) => {
+  const formElement = getByTestId("dropzone");
+
+  // Define files property on Dropzone element and fire drop event
+  Object.defineProperty(formElement, "files", { value: [file] });
+  fireEvent.drop(formElement)
+}
