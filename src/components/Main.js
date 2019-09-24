d6083b03b0bc79dee2aebee5851b0c24a32513c6
joshma91 <josh.ma91@gmail.com>
2019-09-23 22:10:42 -0700

    add comments to Main.js and empty file error

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,23 +8,40 @@ import Error from "./Error";
 import { sendFile } from "../utils";
 
 const API_URL = "https://wordcount-api.dev.openlaw.io/api/wordcount/v1/upload";
+
 export default function Main() {
+  // Hooks to manage state variables
   const [loading, setLoading] = useState(false);
   const [files, setFiles] = useState(null);
   const [isError, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
   const [fileInfo, setFileInfo] = useState(null);
 
+  // Callback function to handle click, passed into Button
   async function handleAnalyzeClicked() {
+    
+    // Case where file hasn't been selected
     if (!files) {
       setError(true);
       setErrorMessage("Please select a file first");
     } else {
       try {
+        // Upload file and make API call
         setLoading(true);
         const fileInfo = await sendFile(API_URL, files[0]);
-        setError(false);
-        setFileInfo(fileInfo);
+
+        // Valid result with a wordcount in the response
+        if (fileInfo.totalCount) {
+          setError(false);
+          setFileInfo(fileInfo);
+
+          // Response returns a wordcount of 0
+        } else {
+          setError(true);
+          setErrorMessage("Woops, your file appears to be empty");
+        }
+
+        // Handles server thrown errors
       } catch {
         setError(true);
         setErrorMessage("There seems to be a problem with reading your file");
@@ -43,16 +60,10 @@ export default function Main() {
           backSpeed={50}
           startDelay={200}
           backDelay={1200}
-          cursorChar={"|"}
         />
       </Header>
       <Dropzone setFiles={setFiles} />
-      <SubmitButton
-        onClick={() => {
-          handleAnalyzeClicked();
-        }}
-        loading={loading}
-      />
+      <SubmitButton onClick={() => handleAnalyzeClicked()} loading={loading} />
       <Error isError={isError} errorMessage={errorMessage} />
       <Table fileInfo={fileInfo} />
     </AppWrapper>
