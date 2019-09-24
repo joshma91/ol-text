ff926c022d82c3b6ff8b9c1bec45e5fe45fe577a
joshma91 <josh.ma91@gmail.com>
2019-09-23 23:32:29 -0700

    add test case for non-ascii file in main.test.js

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -16,9 +16,7 @@ describe("Testing Main component", () => {
     expect(queryByText("Please select a file first")).toBeInTheDocument();
   });
 
-
   test("user uploads valid text file", async () => {
-    
     const file = new File(["test"], "some.txt", { type: "text/plain" });
 
     const { getByText, getByTestId } = render(<Main />);
@@ -33,19 +31,36 @@ describe("Testing Main component", () => {
 
     // Wait for DOM change
     await waitForElement(() => getByText(/total words/i));
-
     expect(getByText(/total words/i)).toBeInTheDocument();
+  });
+
+  test("user uploads text file with non-ascii characters", async () => {
+    const file = new File(["♭"], "some.txt", { type: "text/plain" });
+
+    const { getByText, getByTestId } = render(<Main />);
+
+    // Wrapping in await act because of React bug
+    await act(async () => {
+      simulateFileDrop(getByTestId, file);
+    });
+
+    // Simulate click and call API
+    fireEvent.click(getByText(/analyz/i).closest("button"));
+
+    // Wait for DOM change
+    await waitForElement(() => getByText(/appears to be empty/i));
+    expect(getByText(/appears to be empty/i)).toBeInTheDocument();
   });
 
   test("user uploads a file that is rejected by the API", async () => {
     // Disable console.error for this test
-    console.error = jest.fn()
+    console.error = jest.fn();
 
-    // Mock fetch response error 
-    global.fetch = jest.fn()
-    global.fetch.mockImplementation(()=> {
-      throw new Error()
-    })
+    // Mock fetch response error
+    global.fetch = jest.fn();
+    global.fetch.mockImplementation(() => {
+      throw new Error();
+    });
     const file = new File(["test"], "some.txt", { type: "text/plain" });
 
     const { getByText, getByTestId } = render(<Main />);
@@ -59,7 +74,6 @@ describe("Testing Main component", () => {
     fireEvent.click(getByText(/analyz/i).closest("button"));
 
     await waitForElement(() => getByText(/There seems to be a problem/i));
-
     expect(getByText(/There seems to be a problem/i)).toBeInTheDocument();
   });
 });
